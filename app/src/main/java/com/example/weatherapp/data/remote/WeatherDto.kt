63821199fedf490325c286e519b676904a92da81
alperen-sakin <azapkaan@gmail.com>
2025-10-24 07:44:21 +0200

    refactor: removed @field

--- app/src/main/java/com/example/weatherapp/data/remote/WeatherDto.kt
+++ app/src/main/java/com/example/weatherapp/data/remote/WeatherDto.kt
@@ -3,6 +3,6 @@ package com.example.weatherapp.data.remote
 import com.squareup.moshi.Json
 
 data class WeatherDto(
-    @field:Json(name = "hourly")
+    @Json(name = "hourly")
     val weatherData: WeatherDataDto
 )
