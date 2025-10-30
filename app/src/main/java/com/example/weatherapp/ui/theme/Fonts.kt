23a6606fae5c1c429203adfcdd9b133367fc814b
alperen-sakin <azapkaan@gmail.com>
2025-10-30 04:14:33 +0100

    prep: add Font Family resources

---
+++ app/src/main/java/com/example/weatherapp/ui/theme/Fonts.kt
@@ -0,0 +1,14 @@
+package com.example.weatherapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.weatherapp.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+
+)
