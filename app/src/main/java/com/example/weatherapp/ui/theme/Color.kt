bd30da73b0a6b999ce85da5b0b6b15307b11ac15
alperen-sakin <azapkaan@gmail.com>
2025-10-31 05:25:47 +0100

    prep: Add Color and String Resources

--- app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -11,3 +11,10 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val WeatherInActiveCellBackgroundColor = Color(0xFF48319D)
+
+val SelectedTabIndicatorColor = Color(0xFF009DFF)
+val GradientIndicatorColor = listOf(Color.Transparent, SelectedTabIndicatorColor, Color.Transparent)
+
+val ForecastCurrentDayColor = Color(0xFF2B85D4)
+
+val ForecastColor = Color(0xff194B78)
